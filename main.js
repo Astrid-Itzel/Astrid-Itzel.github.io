5f48c3c45b6ed2598682106b18b2bb1cc291b6f5
Astrid Alcántar <astrid.itzel.alza@gmail.com>
2024-11-26 20:47:40 -0600

    Primera carga de archivos de portafolio

---
+++ main.js
@@ -0,0 +1,19 @@
+/*Type writer:  https://safi.me.uk/typewriterjs/ z*/
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Front-end developer Jr.')
+    .pauseFor(2500)
+    //después de pausar, se elimina y escribe lo siguiente
+    .deleteAll()
+    .typeString("Where there's a will, there's a way")
+    .pauseFor(2500)
+    //Número de caracteres que se borrarán
+    .deleteChars(7)
+    .typeString('<strong>code!</strong>')
+    .pauseFor(2500)
+    .start();
